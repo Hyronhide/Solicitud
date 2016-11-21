14d1bf5a8dbb4b79608156305c875ecb6f724996
Hyronhide <drmosquera90@misena.edu.co>
2016-11-21 10:06:02 -0500

    1.0.1

--- solicitud/solicitud/apps/formulario/views.py
+++ solicitud/solicitud/apps/formulario/views.py
@@ -55,8 +55,8 @@ def Solicitud_View (request):
 				servicio_usuario = "Contenidos programaticos: valor $4.100, Entregar a Luz Marina Ríos"		 	 
 
 			'''Bloque configuracion de envio por GMAIL'''
-			#to_admin = 'lgonzalez21@misena.edu.co'
-			to_admin = 'drmosquera90@misena.edu.co'
+			to_admin = 'lgonzalez21@misena.edu.co'
+			#o_admin = 'drmosquera90@misena.edu.co'
 			to_user = correo
 			html_content_admin = "<p><b>Solicitud de servicio: </b>%s</p><br> <b>Nombres</b>: %s <br><br> <b>Apellidos</b>: %s  <br><br> <b>Correo:</b> %s  <br><br> <b>Cedula:</b> %s  <br><br> <b>Telefono:</b> %s "%(servicio,nombres,apellidos,correo,cedula,telefono)
 			html_content_user = "<p><b>Solicitud de servicio: </b>%s</p><br> -Debe imprimir únicamente copia de banco.<br>-Debe hacer consignación en Bancolombia, no corresponsales bancarios.<br>-Una vez consigne o cancele su recibo debe hacerlos llegar a las oficinas de coordinación académica según su solicitud ,en este caso:  %s.<br>-La consignación debe hacerse el mismo día que se genera el recibo.<br>-Debe imprimir el recibo en impresora laser.<br>"%(servicio,servicio_usuario)
@@ -64,9 +64,10 @@ def Solicitud_View (request):
 			msg = EmailMultiAlternatives('Solicitud de recibo de consignacion', html_content_admin, 'from@gmail.com',[to_admin])
 			msg2 = EmailMultiAlternatives('Solicitud de recibo de consignacion (Recuerde esto al momento de obtener el recibo)', html_content_user, 'from@gmail.com',[to_user])
 			msg.attach_alternative(html_content_admin,'text/html')
-			msg.send()
+			
 			msg2.attach_alternative(html_content_user,'text/html')
 			msg2.send()
+			msg.send()
 			'''Fin del bloque'''
 	else:
 		formulario = solicitud_form()		
