7855de409800e5b2901da8f4bea74dc42c60fe1f
Hyronhide <drmosquera90@misena.edu.co>
2016-12-02 14:08:09 -0500

    hero 15

--- solicitud/solicitud/settings.py
+++ solicitud/solicitud/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Solicitud/solicitud/static"),
+    os.path.join(BASE_DIR, 'Solicitud/solicitud/static'),
 ]
 
 STATIC_ROOT = 'static'
