6efc43300fb6446840b15ff2ba8e3b35d0def54c
Hyronhide <drmosquera90@misena.edu.co>
2016-12-02 11:01:56 -0500

    heroku

--- solicitud/solicitud/settings.py
+++ solicitud/solicitud/settings.py
@@ -80,12 +80,18 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'solicitud',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'solicitud',
+        #'USER': 'root',
+        #'PASSWORD': '',
+        #'HOST': '127.0.0.1',
+        #'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'solicitud_db',
+        'USER': 'django-windows',
+        'PASSWORD': 'b6032drxw1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
